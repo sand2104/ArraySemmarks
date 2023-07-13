5d45f0ef56a27793166fa82f53fa0da0bac6173c
sand2104 <prasadsandy2000@gmail.com>
2023-07-13 12:03:12 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayMarksofStudent
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] marks = new int[4, 3];
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine($"Enter Student {i + 1} \'s Roll Number");
+                marks[i, 0] = int.Parse(Console.ReadLine());
+                for (int j = 1; j < 3; j++)
+                {
+                    Console.WriteLine($"Enter marks of Student\'s {i + 1} in Sem {j}");
+                    marks[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+            Console.WriteLine("Marks List as follovs");
+            Console.WriteLine("RollNo\t SemOne\t SemTwo");
+            for (int i = 0; 1 < 4; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(marks[i, j] + "\t");
+                }
+                Console.WriteLine("\n");
+            }
+                Console.ReadKey();
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ArrayMarksofStudent")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ArrayMarksofStudent")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4052a25f-d167-4e84-93ff-3e1010bba69c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
